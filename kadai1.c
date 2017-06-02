dca2ca93d9333c606bdd58089b00f1fef976cef8
CTKyamamoto <masaya-yamamoto@courage.co.jp>
2017-06-02 13:58:14 +0900

    課題1、2アップロード

---
+++ kadai1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int i;
+	int total; /* 合計 */
+	
+	total = 0;
+	
+	/* 1から10まで加算する処理 */
+	for( i=1; i<=10; i++)
+	{
+		total += i;
+	}
+	
+	/* 合計を表示する */
+	printf("合計 = %d\n" , total);
+	
+	return 0;
+}
---
+++ kadai2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+	char c;
+	
+	c = 'A';
+	
+	printf("%c\n", c); /* %c=文字として表示 */
+	printf("%d\n", c); /* %d=10進数として表示 */
+	printf("%x\n", c); /* %x=16進数として表示 */
+}
